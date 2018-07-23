6aede5b57dd083018687a485457e7880bf4ae8c5
Jean Malan <jeanmalan22@gmail.com>
2018-07-23 03:55:47 +0000

    home page modify

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,6 +5,15 @@ class IndexController < ApplicationController
       @purchases = Purchase.all.where("user_id =?", current_user.id)
       @transactions = FinancialTransaction.all.where("user_id =?", current_user.id)
       @sale_total = Sale.where('date > ?', 30.days.ago).where("user_id =?", current_user.id)
+      @purchases = Purchase.where('date > ?', 30.days.ago).where("user_id =?", current_user.id)
+      
+      @gross_profit = 0
+      @sale.each do |sale|
+          @gross_profit += sale.amount.to_f
+      end
+      @purchases.each do |purchase|
+          @gross_profit += purchase.amount.to_f
+      end
       
  end
 end
